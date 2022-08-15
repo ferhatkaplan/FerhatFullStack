e3f6d6a2d67a8afb0bfc10ff97ded978f70569dc
ferhatkaplan <ferhatkaplan2742@gmail.com>
2022-08-15 21:20:14 +0300

    string ve var

---
+++ frontend/tutorials/5_JavaScript/template.js
@@ -0,0 +1,34 @@
+// alert("deneme") => single comment
+/*
+multiple comment
+ */
+
+//variable(değişken)
+//"var" dinamik bir değişkendir.
+var kalem = "dijital kalem";
+console.log(kalem)
+
+//değişken tanımlamak
+var x1 = 45;
+var x2 = 66;
+var x3 = x1 + x2;
+console.log(x3)
+
+//değişken tanımlamak (readibility)
+var x4=45,x5=15,x6;
+x6=x4+x5;
+console.log(x6)
+
+//variable information  =>> white space (boşluklara dikkat et)
+var temp = 44;
+console.log(temp)
+
+//veriable data type
+var kelime=27; // sayı.
+kelime="SELAMLAR"; //string
+kelime=true; //boolean
+console.log(kelime)
+
+//veriable common rules 
+
+
