ccda6e572b3bef26f20bf339f2e1cc8790750b9d
ferhatkaplan <ferhatkaplan2742@gmail.com>
2022-08-16 21:21:52 +0300

    js2

--- frontend/tutorials/5_JavaScript/template.js
+++ frontend/tutorials/5_JavaScript/template.js
@@ -8,27 +8,52 @@ multiple comment
 var kalem = "dijital kalem";
 console.log(kalem)
 
+console.log("******************")
+
 //değişken tanımlamak
 var x1 = 45;
 var x2 = 66;
 var x3 = x1 + x2;
 console.log(x3)
 
+console.log("******************")
+
 //değişken tanımlamak (readibility)
 var x4=45,x5=15,x6;
 x6=x4+x5;
 console.log(x6)
 
+console.log("******************")
+
 //variable information  =>> white space (boşluklara dikkat et)
 var temp = 44;
 console.log(temp)
 
+console.log("******************")
+//whoisting: önce değeri yaz sonra veri türünü yaz
+deneme=27+93;
+var deneme;
+console.log(deneme)
+
+console.log("******************")
+
 //veriable data type
 var kelime=27; // sayı.
-kelime="SELAMLAR"; //string
+kelime="selam"; //string
 kelime=true; //boolean
 console.log(kelime)
 
-//veriable common rules 
+console.log("******************")
+
+// variable typeof: türü nedir?
+var kelime=27;
+console.log(typeof kelime)
+kelime="selam"; //string
+console.log(typeof kelime)
+kelime=true;
+console.log(typeof kelime)
+console.log(kelime)
+
+
 
 
